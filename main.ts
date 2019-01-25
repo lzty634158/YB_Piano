f9e116b4dcecbce00b57593dceec493b50df69bf
lzty634158 <lzty634158@github.com>
2019-01-25 12:22:42 +0800

    fix touch

--- main.ts
+++ main.ts
@@ -38,37 +38,39 @@ namespace YB_Piano {
         power_down
     }
 
-   export enum touch {
+    export enum touch {
+        //% blockId="None" block="None"
+        None = 0x0000,
         //% blockId="C" block="C"
-        C = 4,
+        C = 0x0004,
         //% blockId="CD" block="CD"
-        CD = 8,
+        CD = 0x0008,
         //% blockId="D" block="D"
-        D = 16,
+        D = 0x0010,
         //% blockId="DE" block="DE"
-        DE = 32,
+        DE = 0x0020,
         //% blockId="E" block="E"
-        E = 64,
+        E = 0x0040,
         //% blockId="F" block="F"
-        F = 128,
+        F = 0x0080,
         //% blockId="FG" block="FG"
-        FG = 256,
+        FG = 0x0100,
         //% blockId="G" block="G"
-        G = 512,
+        G = 0x0200,
         //% blockId="GA" block="GA"
-        GA = 1024,
+        GA = 0x0400,
         //% blockId="A" block="A"
-        A = 2048,
+        A = 0x0800,
         //% blockId="AB" block="AB"
-        AB = 4096,
+        AB = 0x1000,
         //% blockId="B" block="B"
-        B = 8192, 
+        B = 0x2000, 
         //% blockId="L" block="L"
-        L = 2,
+        L = 0x0002,
         //% blockId="M" block="M"
-        M = 1,
+        M = 0x0001,
         //% blockId="H" block="H"
-        H = 16384,
+        H = 0x4000,
        
     }
     
@@ -186,9 +188,123 @@ namespace YB_Piano {
         let c = value;
         return c;
     }
+
+    //% blockId=YB_Piano_PlayPiano block="Play Piano|tone %value"
+    //% weight=95
+    //% blockGap=10
+    //% color="#17ecc1"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
+    export function PlayPiano(value: number): void {
+        let a = 0;
+        let b = 0;
+        let c = 0;
+        let temp = 0;
+        pins.i2cWriteNumber(0x50,8,NumberFormat.UInt8BE,false);
+        a = pins.i2cReadNumber(0x50, NumberFormat.UInt8BE, true);
+        b = pins.i2cReadNumber(0x50, NumberFormat.UInt8BE, false);
+        c = (b << 8) | a;
+        
+        if (value == 1) { 
+            if ((c & play) != 0) {
+                c = c & temp;
+            } else if (c & touch.C) {
+                music.ringTone(131);
+            } else if (c & touch.CD) {
+                music.ringTone(139);
+            } else if (c & touch.D) {
+                music.ringTone(147);
+            } else if (c & touch.DE) {
+                music.ringTone(156);
+            } else if (c & touch.E) {
+                music.ringTone(165);
+            } else if (c & touch.F) {
+                music.ringTone(175);
+            } else if (c & touch.FG) {
+                music.ringTone(185);
+            } else if (c & touch.G) {
+                music.ringTone(196);
+            } else if (c & touch.GA) {
+                music.ringTone(208);
+            } else if (c & touch.A) {
+                music.ringTone(220);
+            } else if (c & touch.AB) {
+                music.ringTone(233);
+            } else if (c & touch.B) {
+                music.ringTone(247);
+            } else if (c == touch.None) {
+                music.ringTone(0);
+            }
+        }
+        else  if (value == 2) { 
+            if ((c & play) != 0) {
+                c = c & temp;
+            } else if (c & touch.C) {
+                music.ringTone(262);
+            } else if (c & touch.CD) {
+                music.ringTone(277);
+            } else if (c & touch.D) {
+                music.ringTone(294);
+            } else if (c & touch.DE) {
+                music.ringTone(311);
+            } else if (c & touch.E) {
+                music.ringTone(330);
+            } else if (c & touch.F) {
+                music.ringTone(349);
+            } else if (c & touch.FG) {
+                music.ringTone(370);
+            } else if (c & touch.G) {
+                music.ringTone(392);
+            } else if (c & touch.GA) {
+                music.ringTone(415);
+            } else if (c & touch.A) {
+                music.ringTone(440);
+            } else if (c & touch.AB) {
+                music.ringTone(466);
+            } else if (c & touch.B) {
+                music.ringTone(494);
+            } else if (c == touch.None) {
+                music.ringTone(0);
+            }
+        }
+        else  if (value == 3) { 
+            if ((c & play) != 0) {
+                c = c & temp;
+            } else if (c & touch.C) {
+                music.ringTone(523);
+            } else if (c & touch.CD) {
+                music.ringTone(554);
+            } else if (c & touch.D) {
+                music.ringTone(587);
+            } else if (c & touch.DE) {
+                music.ringTone(622);
+            } else if (c & touch.E) {
+                music.ringTone(659);
+            } else if (c & touch.F) {
+                music.ringTone(698);
+            } else if (c & touch.FG) {
+                music.ringTone(740);
+            } else if (c & touch.G) {
+                music.ringTone(784);
+            } else if (c & touch.GA) {
+                music.ringTone(831);
+            } else if (c & touch.A) {
+                music.ringTone(880);
+            } else if (c & touch.AB) {
+                music.ringTone(932);
+            } else if (c & touch.B) {
+                music.ringTone(988);
+            } else if (c == touch.None) {
+                music.ringTone(0);
+            }
+        }
+        
+        
+
+        
+    }
     
     //% blockId=YB_Piano_TouchSensitivity block="TouchSensitivity|%key %value"
-    //% weight=95
+    //% weight=94
     //% blockGap=10
     //% color="#17ecc1"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
@@ -260,7 +376,7 @@ namespace YB_Piano {
     }
 
     //% blockId=YB_Piano_TouchSensitivityValue block="TouchSensitivityValue|%key"
-    //% weight=94
+    //% weight=93
     //% blockGap=10
     //% color="#17ecc1"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
